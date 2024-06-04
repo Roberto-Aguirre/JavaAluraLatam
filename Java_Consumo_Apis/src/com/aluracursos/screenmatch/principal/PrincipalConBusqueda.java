73a3001dc472951228439a78474ff08a3c768afd
Roberto-Aguirre <ivanaguirre02@hotmail.com>
2024-06-03 19:27:39 -0600

    Ahora el usuario puede buscar la pelicula

--- Java_Consumo_Apis/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
+++ Java_Consumo_Apis/src/com/aluracursos/screenmatch/principal/PrincipalConBusqueda.java
@@ -6,14 +6,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class PrincipalConBusqueda {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ingrese la pelicula a buscar: ");
+        var peliculaUsuario = input.nextLine();
 
+        String peliculaPeticion = "http://www.omdbapi.com/?t="+peliculaUsuario+"&apikey=57bedfc9";
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=the_matrix&apikey=57bedfc9"))
+                .uri(URI.create(peliculaPeticion))
                 .build();
         
          HttpResponse<String> response = client.send(request, BodyHandlers.ofString()); 
